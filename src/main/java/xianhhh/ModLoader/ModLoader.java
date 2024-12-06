f0e7d35560860f329d408f96ca93f3f4e149dcfd
xianhhh <zeh5741@163.com>
2024-12-06 19:14:10 +0800

    modLoader

---
+++ src/main/java/xianhhh/ModLoader/ModLoader.java
@@ -0,0 +1,14 @@
+package xianhhh.ModLoader;
+
+import xianhhh.Utils.ResourcesHelper.ResourcesUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.jar.JarFile;
+
+public class ModLoader {
+    public void load(){
+       ArrayList<JarFile> mods = ResourcesUtils.getModJars();
+       
+    }
+}
