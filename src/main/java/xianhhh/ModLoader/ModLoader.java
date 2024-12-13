fe1af55e92bd993979f500984eb2c41b0bc3541d
xianhhh <zeh5741@163.com>
2024-12-13 20:38:05 +0800

    Annotation + ModLoader

--- src/main/java/xianhhh/ModLoader/ModLoader.java
+++ src/main/java/xianhhh/ModLoader/ModLoader.java
@@ -1,14 +1,45 @@
 package xianhhh.ModLoader;
 
+import xianhhh.ModLoader.Annotation.ModLoad;
+import xianhhh.ModLoader.Annotation.ModMain;
 import xianhhh.Utils.ResourcesHelper.ResourcesUtils;
 
-import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class ModLoader {
     public void load(){
        ArrayList<JarFile> mods = ResourcesUtils.getModJars();
-       
+       ResourcesUtils.createDictionary();
+
+       if(mods.isEmpty()){
+           return;
+       }
+       for(JarFile jarFile : mods){
+           try {
+               // 打开 JAR 文件
+
+               // 遍历 JAR 文件中的所有条目
+               Enumeration<JarEntry> entries = jarFile.entries();
+               while (entries.hasMoreElements()) {
+                   JarEntry entry = entries.nextElement();
+
+                   // 只处理以 ".class" 结尾的条目
+                   if (entry.getName().endsWith(".class")) {
+                       String className = entry.getName().replace('/', '.').substring(0, entry.getName().length() - 6);
+                       Class<?> mainClass = Class.forName(className);
+                       ResourcesUtils.isModHasMainAndLoadMDAndInvoke(mainClass, ModMain.class, ModLoad.class);
+                   }
+               }
+
+               // 关闭 JAR 文件
+               jarFile.close();
+           } catch (Exception e) {
+               e.printStackTrace();
+           }
+       }
     }
 }
--- src/main/java/xianhhh/Utils/ResourcesHelper/ResourcesUtils.java
+++ src/main/java/xianhhh/Utils/ResourcesHelper/ResourcesUtils.java
@@ -1,19 +1,16 @@
 package xianhhh.Utils.ResourcesHelper;
+
 import java.io.*;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class ResourcesUtils {
     private static final File main = new File("C:\\DickClient")
             ,mod = new File("C:\\DickClient\\Mods");
 
-    private static void createDictionary() {
+    public static void createDictionary() {
         if (!main.exists())
             main.mkdir();
         if (!mod.exists()) {
@@ -26,7 +23,7 @@ public class ResourcesUtils {
         File[] files = mod.listFiles();
         for(File f : files){
             int length = f.getName().split("\\.").length;
-            if(f.getName().split("\\.")[length].equalsIgnoreCase("jar")){
+            if(f.getName().split("\\.")[length-1].equalsIgnoreCase("jar")){
                 list.add(f);
             }
         }
@@ -44,5 +41,24 @@ public class ResourcesUtils {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static void isModHasMainAndLoadMDAndInvoke(Class<?> clazz, Class<? extends Annotation> mainAn, Class<? extends Annotation> modloadAn){
+        try {
+            ArrayList<Method> mts = new ArrayList<Method>();
+            if (clazz.isAnnotationPresent(mainAn)) {
+                for (Method mt : clazz.getDeclaredMethods()) {
+                    mt.setAccessible(true);
+                    if (mt.isAnnotationPresent(modloadAn)) {
+                        mts.add(mt);
+                    }
+                }
+                for (Method mt : mts) {
+                    mt.invoke(clazz, (Object) null);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
